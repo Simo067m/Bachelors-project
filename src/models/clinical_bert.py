63437f642fcc57cb7dd7db43c131d8c3f82d228b
Simon Stenbæk Jensen <s214592@student.dtu.dk>
2024-03-19 14:45:08 +0100

    Update bio-clinical-BERT with encode and embed methods

--- src/models/clinical_bert.py
+++ src/models/clinical_bert.py
@@ -1,26 +1,9 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
+import time
 
 # Disable cache warning
 HF_HUB_DISABLE_SYMLINKS_WARNING = True
-
-# Load the model
-tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-
-sentence = "The patient is in danger of suffering a heart attack."
-
-encoded_input = tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
-
-print(encoded_input)
-
-with torch.no_grad():
-    output = model(**encoded_input)
-    embeddings = output.last_hidden_state.mean(dim=1)
-
-print(embeddings)
-print(len(embeddings[0]))
-print(len(sentence))
 
 class bio_clinical_BERT:
     """
@@ -31,3 +14,41 @@ class bio_clinical_BERT:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
         self.model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
+    
+    def encode(self, text : list, add_special_tokens : bool):
+        """
+        Returns the encodings of the text input.
+        """
+        start_time = time.time()
+
+        encoded_output = self.tokenizer(text, padding=True, truncation=True, return_tensors="pt", 
+                                        return_attention_mask=True, return_token_type_ids=True,
+                                        max_length=512, add_special_tokens=add_special_tokens)
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print(f"Finished encoding text in {minutes} minutes and {seconds} seconds.")
+
+        return encoded_output
+    
+    def embed(self, encoded_output):
+        """
+        Returns the embeddings of the encoded output.
+        """
+
+        start_time = time.time()
+
+        with torch.no_grad():
+            output = self.model(**encoded_output)
+            embeddings = output.last_hidden_state.mean(dim=1)
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        minutes = int(elapsed_time // 60)
+        seconds = int(elapsed_time % 60)
+        print(f"Finished embedding text in {minutes} minutes and {seconds} seconds.")
+
+        return embeddings
+    
