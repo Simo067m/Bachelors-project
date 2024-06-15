1c8093a2b8335340be2d4c100779c27aea9a8b6f
Simon Stenbæk Jensen <s214592@student.dtu.dk>
2024-06-15 23:14:07 +0200

    Use correct path

--- src/configs/ptb_configs.py
+++ src/configs/ptb_configs.py
@@ -9,8 +9,8 @@ class Configs(object):
         self.num_classes = 5
         self.num_classes_disease = 2
     
-        #self.path_to_dataset = os.path.join(os.getcwd(), "Datasets", "ptb", "")
-        self.path_to_dataset = os.path.join("c:\\", "Users", "ssjsi", "Documents", "Bachelors-project", "Datasets", "ptb", "")
+        self.path_to_dataset = os.path.join(os.getcwd(), "Datasets", "ptb", "")
+        #self.path_to_dataset = os.path.join("c:\\", "Users", "ssjsi", "Documents", "Bachelors-project", "Datasets", "ptb", "")
 
         self.learning_rate = 0.002 # From "ETP: Learning Transferable ECG Representations via ECG-Text Pre-Training" (https://arxiv.org/pdf/2309.07145.pdf) section 3.2
         self.weight_decay = 0.00001 # From "ETP: Learning Transferable ECG Representations via ECG-Text Pre-Training" (https://arxiv.org/pdf/2309.07145.pdf) section 3.2
